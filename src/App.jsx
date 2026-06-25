974e9ed6e20d86b437b409b47776017ee73ed126
sarthaksinghaniya <sarthaksinghaniya789@gmail.com>
2026-06-25 12:11:31 +0530

    logic building

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Preloader from './components/Preloader'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Expertise from './components/Expertise'
+import Skills from './components/Skills'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Preloader />
+      <Navbar />
+      <Hero />
+      <About />
+      <Expertise />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
